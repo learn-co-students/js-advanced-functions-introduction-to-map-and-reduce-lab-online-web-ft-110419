d3deadc47865ebfefa906e491c3095772f5bc587
github username <github email address>
2020-03-04 16:31:07 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Your code here
+function mapToNegativize(sourceArray){
+  let array = []
+  sourceArray.forEach(num =>{
+    array.push(num * -1)
+  })
+  return array
+}
+
+function mapToNoChange(sourceArray){
+  let array = []
+  sourceArray.forEach(string =>{
+    array.push(string)
+  })
+  return array
+}
+
+function mapToDouble(sourceArray){
+  let array = []
+  sourceArray.forEach(num => {
+    array.push(num * 2)
+  })
+  return array
+}
+
+function mapToSquare(sourceArray){
+  let array = []
+  sourceArray.forEach(num =>{
+    array.push(num * num)
+  })
+  return array
+}
+
+function reduceToTotal(sourceArray, startingPoint = 0){
+  sourceArray.forEach(num =>{
+    startingPoint += num
+  })
+  return startingPoint
+}
+
+function reduceToAllTrue(sourceArray){
+  let array = []
+  sourceArray.forEach(elem =>{
+    array.push(!!elem)
+  })
+  return !array.includes(false) ? true : false
+}
+
+function reduceToAnyTrue(sourceArray){
+  let array = []
+  sourceArray.forEach(elem =>{
+    array.push(!!elem)
+  })
+  return array.includes(true) ? true : false
+}
