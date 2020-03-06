313441b3a66cd47d02467f1f5c07abbce3c9dde4
Ethan <ethan_gustafson@outlook.com>
2020-03-06 13:19:28 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
 // Your code here
+function mapToNegativize(array) {
+    let a = []
+    for (let i = 0; i < array.length; i++ ) {
+      a.push(-1 * array[i]) // Unique work
+    }
+    return a
+}
+// function mapToNegativize(array){
+//     return array.map(element => element * -1);
+// } 
+function mapToNoChange(array) {
+    let a = []
+    for (let i = 0; i < array.length; i++ ) {
+      a.push(array[i]) // Unique work
+    }
+    return a
+}
+// function mapToNoChange(array){
+//     return array;
+// }
+function mapToDouble(array) {
+    let a = []
+    for (let i = 0; i < array.length; i++ ) {
+      a.push(2 * array[i]) // Unique work
+    }
+    return a
+}
+// function mapToDouble(array){
+//     return array.map(element => element * 2);
+// }
+function mapToSquare(array) {
+    let a = []
+    for (let i = 0; i < array.length; i++ ) {
+      a.push(array[i] * array[i]) // Unique work
+    }
+    return a
+}
+// function mapToSquare(array){
+//     return array.map(element => element ** 2);
+// }
+function reduceToTotal(array, int=0) {
+    for (let i = 0; i < array.length; i++){
+        int+= array[i]
+    }
+    return int
+}
+
+function reduceToAllTrue(array){
+    for (let i = 0; i < array.length; i++){
+        if (!array[i]) return false
+    }
+    return true
+}
+
+function reduceToAnyTrue(array){
+    for (let i = 0; i < array.length; i++){
+        if (array[i]) return true
+    }
+    return false
+}
